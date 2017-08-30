99ea168017d5c2f7ec11d16f3c33d02c063ef0e5
Mikkel Ricky <rimi@aarhus.dk>
2017-08-30 11:38:22 +0200

    ICAP-9: Added full width display of content

--- section-parts/section-about.php
+++ section-parts/section-about.php
@@ -10,6 +10,41 @@ $classes = 'screenr-section section-about section-padding section-padding-larger
 <?php } ?>
     <div class="container">
         <div class="row">
+          <?php
+          $has_content = $title || $subtitle || $desc;
+          $has_page = get_theme_mod('about_page_id') && get_post(get_theme_mod('about_page_id'));
+          ?>
+
+          <?php if ($has_content && !$has_page): ?>
+                <div class="section-title-area">
+                    <?php if ( $subtitle ) { ?><div class="section-subtitle"><?php echo esc_html( $subtitle ); ?></div><?php } ?>
+                    <?php if ( $title ) { ?><h2 class="section-title"><?php echo esc_html( $title ); ?></h2><?php } ?>
+                    <?php if ( $desc ) { ?><div class="section-desc"><?php echo apply_filters( 'screenr_content_text', $desc ); ?></div><?php } ?>
+                </div>
+          <?php elseif (!$has_content && $has_page): ?>
+            <?php
+            $page_id = get_theme_mod( 'about_page_id' );
+            if ( $page_id && ( $post = get_post( $page_id ) )) {
+                setup_postdata( $post );
+                ?>
+                <div class="col-md-7">
+                    <div class="section-about-content">
+                        <?php
+                        if (  get_theme_mod( 'about_page_content_type' ) == 'content' ) {
+                            the_content();
+                        } else {
+                            the_excerpt();
+                        }
+                        ?>
+                    </div>
+                </div>
+                <?php
+            }
+            wp_reset_postdata();
+                ?>
+
+          <?php else: ?>
+
             <div class="col-md-5">
                 <?php if (  $title || $subtitle || $desc ) { ?>
                 <div class="section-title-area">
@@ -40,7 +75,7 @@ $classes = 'screenr-section section-about section-padding section-padding-larger
             }
             wp_reset_postdata();
             ?>
-
+					<?php endif ?>
         </div>
     </div>
 <?php if ( ! screenr_is_selective_refresh() ) { ?>
